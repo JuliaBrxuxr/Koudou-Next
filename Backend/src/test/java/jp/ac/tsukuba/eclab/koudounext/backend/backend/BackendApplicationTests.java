47b8d1d1774cd8d55dc730f598f76b2e586a6282
UtopiaXC <utopiaxc1025@gmail.com>
2025-04-10 13:59:37 +0900

    Title: Create project. Details: Create project and build up the structure. Type: Structure. Test: N/A.

---
+++ Backend/src/test/java/jp/ac/tsukuba/eclab/koudounext/backend/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.tsukuba.eclab.koudounext.backend.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
