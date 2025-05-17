34f0690b29bae94893ea532720c8341590d0eade
JuliaBrxuxr <123084407+JuliaBrxuxr@users.noreply.github.com>
2025-05-17 05:09:13 +0200

    creation of map and sample agents

--- Frontend/koudou-next-react/src/App.tsx
+++ Frontend/koudou-next-react/src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "leaflet/dist/leaflet.css";
 
+// imports for map features
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import L from "leaflet";
 function App() {
-  const [count, setCount] = useState(0)
+  type agentMarkerData = {
+    geocode: [number, number];
+    popUp: string;
+  };
+  const markers: agentMarkerData[] = [
+    {
+      geocode: [36.1114, 140.1038],
+      popUp: "Agent 1", //html text
+    },
+
+    {
+      geocode: [36.0763, 140.1068],
+      popUp: "Agent 2",
+    },
+
+    {
+      geocode: [36.0835, 140.0766],
+      popUp: "Agent 3",
+    },
+  ];
+
+  // you did n
+
+  const agentIcon = L.icon({
+    iconUrl: "/icons/cookie-man.svg",
+    iconSize: [38, 38],
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MapContainer
+        // center={[48.7793, 9.1773]} // for testing purposes coordinates of my home region
+        center={[36.0924, 139.9644]} // coordinates Tsukuba-shi
+        zoom={15}
+      >
+        <TileLayer
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {markers.map((marker) => (
+          <Marker position={marker.geocode} icon={agentIcon}>
+            <Popup>{marker.popUp}</Popup>
+          </Marker>
+        ))}
+        {/*    /** <IconCookieManFilled />; */}
+      </MapContainer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
