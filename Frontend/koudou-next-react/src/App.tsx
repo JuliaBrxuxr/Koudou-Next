b93cab4ccc2b214880685ae00a94af5edbd2b39f
JuliaBrxuxr <123084407+JuliaBrxuxr@users.noreply.github.com>
2025-05-29 09:26:14 +0200

    UI: add agents on map with mouse click

--- Frontend/koudou-next-react/src/App.tsx
+++ Frontend/koudou-next-react/src/App.tsx
@@ -1,18 +1,45 @@
 import "./App.css";
 import "leaflet/dist/leaflet.css";
 
-
+import { useState } from "react";
 
 // imports for map features
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
 
 // import for Sidebar
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/comps/app-sidebar";
 import { Button } from "@/components/ui/button";
+import { LatLng } from "leaflet";
 
 import L from "leaflet";
 
+const agentIcon = L.icon({
+  iconUrl: "/icons/cookie-man.svg",
+  iconSize: [38, 38],
+});
+
+function LocationMarker() {
+  const [position, setPosition] = useState<LatLng | null>(null);
+
+  useMapEvents({
+    click(e) {
+      setPosition(e.latlng);
+    },
+  });
+
+  return position === null ? null : (
+    <Marker position={position} icon={agentIcon}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
+}
 
 function App() {
   type agentMarkerData = {
@@ -21,27 +48,20 @@ function App() {
   };
   const markers: agentMarkerData[] = [
     {
-      geocode: [36.1114, 140.1038],
+      geocode: [36.086954, 140.108632],
       popUp: "Agent 1", //html text
     },
 
     {
-      geocode: [36.0763, 140.1068],
+      geocode: [36.10905, 140.101438],
       popUp: "Agent 2",
     },
 
     {
-      geocode: [36.0835, 140.0766],
+      geocode: [36.110438, 140.100199],
       popUp: "Agent 3",
     },
   ];
-
-  const agentIcon = L.icon({
-    iconUrl: "/icons/cookie-man.svg",
-    iconSize: [38, 38],
-  });
-
-   
 
   return (
     <>
@@ -58,11 +78,9 @@ function App() {
             center={[36.0924, 139.9644]} // coordinates Tsukuba-shi
             zoom={15}
           >
-        
-
-          <div className="absolute bottom-9 right-48 z-[999] bg-white text-purple-600">
+            <div className="absolute bottom-9 right-48 z-[999] bg-white text-purple-600">
               <Button variant="outline">Add agents</Button>
-            </div> 
+            </div>
 
             <div className="absolute bottom-9 right-9 z-[9999] bg-purple-600 text-white">
               <Button variant="outline">Start Simulation</Button>
@@ -76,6 +94,7 @@ function App() {
                 <Popup>{marker.popUp}</Popup>
               </Marker>
             ))}
+            <LocationMarker />
           </MapContainer>
         </div>
       </div>
@@ -84,5 +103,3 @@ function App() {
 }
 
 export default App;
-
-
